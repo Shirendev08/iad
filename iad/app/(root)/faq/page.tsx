dee2767481890959af552a7008e9d6ef12f1db0d
Shirendev08 <shirendev.07@gmail.com>
2024-10-28 01:11:27 +0800

    2

--- iad/app/(root)/faq/page.tsx
+++ iad/app/(root)/faq/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import AccordionComponent from '@/components/Accordion'
 const page = () => {
   return (
     <div>
@@ -10,7 +9,6 @@ const page = () => {
         </div>
       <div className='flex justify-center sm:px-16 px-4'>
       <div className='max-w-5xl w-full'>
-      <AccordionComponent/>
     <Footer/>
         </div>
         </div>
