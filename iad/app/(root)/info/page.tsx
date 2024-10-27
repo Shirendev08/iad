9cbb908baf4180d5115106c8731468466047b566
Shirendev08 <shirendev.07@gmail.com>
2024-10-27 20:18:42 +0800

    n

--- iad/app/(root)/info/page.tsx
+++ iad/app/(root)/info/page.tsx
@@ -1,68 +1,59 @@
 import React from 'react'
 import Link from 'next/link'
 import Header from '@/components/Header'
+import Button from '@/components/Button'
+import Footer from '@/components/Footer'
+import Image from 'next/image'
 const page = () => {
-  return (
-    <div className='sm:left-1/4 sm:right-1/4 left-5 right-5'>
-      <Header title="Мэдээлэл" />
-      <div className='w-full h-[15px] mb-8 bg-green-700 text-center items-center justify-center flex'>
-      </div>
-      <div className='flex'>
+    return (
+        <div className='sm:left-1/4 sm:right-1/4 left-5 right-5'>
+            <Header title="Мэдээлэл" />
+            <div className='w-full h-[15px] mb-8 bg-green-700 text-center items-center justify-center flex'>
+            </div>
+            <div className='flex justify-center sm:px-16 px-4'>
+                <div className='max-w-5xl w-full'>
+                    <div className="flex">
 
-<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <Link href="#">
-        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </Link>
-    <div className="p-5">
-        <Link href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        </Link>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <Link href='' key='' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                     
-                        <p>
-                        Салбаруудын үйл ажиллагаатай холбоотой анхаарах зүйлс
-                        </p>
-             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </Link>
-    </div>
-</div>
-<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <Link href="#">
-        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </Link>
-    <div className="p-5">
-        <Link href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        </Link>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <Link href='' key='' className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                     
-                        <p>
-                        Салбаруудын үйл ажиллагаатай холбоотой анхаарах зүйлс
-                        </p>
-             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </Link>
-    </div>
-</div>
-      </div>
+                    <div className='flex mt-10 flex-wrap'>
+                        <div key='' className='bg-white h-30 mb-5'>
 
-<button>
-          Live Chat(Тун удахгүй)
-        </button>
-        <div className='flex flex-col'>
-            Дотоод аудитын газар 2024 <br/>
+                            <div className="">
+                                <Link href='' key='' className="">
+                                    <Image src='/office.jpg' alt='img' width={200} height={100} />
+                                    <p className='text-center'>
+                                        asdjhgsk
+                                    </p>
+                                </Link>
+                            </div>
+                        </div>
 
-[ Bogd-audit@bogdbank.com ]  <br/>
 
-+976 7577 1199 (1007) 
-      </div>
-    </div>
-  )
+                    </div>
+                      <div className='flex mt-10 flex-wrap'>
+                        <div key='' className='bg-white h-30 mb-5'>
+
+                            <div className="">
+                                <Link href='' key='' className="">
+                                    <Image src='/office.jpg' alt='img' width={200} height={100} />
+                                    <p className='text-center'>
+                                        asdjhgsk
+                                    </p>
+                                </Link>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    </div>
+
+                    <Button />
+                    <Footer />
+                </div>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default page
