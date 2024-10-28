f101c65c7cd0eb43710b6e3ff2294f324b22acc3
Shirendev08 <shirendev.07@gmail.com>
2024-10-28 13:43:18 +0800

    menu done

--- iad/components/Header.tsx
+++ iad/components/Header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <header className="relative flex items-center justify-center text-center w-full min-h-[290px] bg-cover bg-center">
     <div className="absolute inset-0 bg-[url('/office.jpg')] bg-cover bg-center opacity-50"></div>
-    <div className="relative z-10  text-5xl font-bold ">
+    <div className="relative z-9  text-5xl font-bold ">
       {title}
     </div>
   </header>
