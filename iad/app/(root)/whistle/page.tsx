96d9df721ac202eecd94a39ba3783377154fecb8
Shirendev08 <shirendev.07@gmail.com>
2024-10-28 08:41:04 +0800

    2

--- iad/app/(root)/whistle/page.tsx
+++ iad/app/(root)/whistle/page.tsx
@@ -11,12 +11,13 @@ const Page = () => {
     information: "",
     file: null, // File state
   });
-
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
+    setSuccess(false)
 
-    console.log("Submitting form...");
-    console.log("Form Data:", formData);
 
    
     const data = {
@@ -25,21 +26,17 @@ const Page = () => {
       information: formData.information,
       file: formData.file
     };
-    console.log(data,"adasd")
 
     try {
-      console.log("Sending email...");
       await sendEmail(data);
-      console.log("Email sent successfully!");
-      alert("Form submitted successfully");
+      setLoading(false);
+      setSuccess(true)
     } catch (error: unknown) { // Specify the type here
-      console.error("Error submitting form:", error);
+      setLoading(false);
   
       // Type guard to handle different types of errors
       if (error instanceof Error) {
-        alert("Failed to submit form: " + error.message);
       } else {
-        alert("Failed to submit form: An unknown error occurred.");
       }
     }
   };
@@ -62,7 +59,15 @@ const Page = () => {
           <ul className="list-disc list-inside dark:text-gray-400 bg-white mb-5">
             {/* Your list items here */}
           </ul>
-
+          {success && (
+            <div
+              className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4"
+              role="alert"
+            >
+              <strong className="font-bold">Амжилттай!</strong>
+              <span className="block sm:inline">Таны маягт амжилттай илгээгдлээ.</span>
+            </div>
+          )}
           <form onSubmit={handleFormSubmit} className="mr-1 mb-10">
             <div className="mb-6">
               <label
@@ -149,9 +154,12 @@ const Page = () => {
 
             <button
               type="submit"
-              className="text-white mt-5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              disabled={loading}
+              className={`text-white mt-5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ${
+                loading ? "cursor-not-allowed opacity-50" : ""
+              }`}
             >
-              Илгээх
+              {loading ? "Илгээж байна..." : "Илгээх"}
             </button>
           </form>
           <Button/>
